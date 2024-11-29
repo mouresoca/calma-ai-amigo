5c1824e9faa8b3e7248bf65745985571e84592ea
mouresoca <169046147+mouresoca@users.noreply.github.com>
2024-11-29 15:38:39 -0300

    apenas a programação

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que comando usamos para fezer o titulo do nosso site?',
+    'seria o <h2> </h2>'
+)
+
+criaCartao(
+    'java script',
+    'Qual seria a sua função?',
+    'desenvolver sites'
+)
+
+criaCartao(
+    'Programação',
+    'pode gerar uma boa renda?',
+    'sim, mas isso dependendo da area onde esta efetuando e também varia da empresa'
+)
+
+criaCartao(
+    'programação',
+    'o que voce deve colocar no inicio do seu codigo?',
+    '<!Doctype html>'
+)
